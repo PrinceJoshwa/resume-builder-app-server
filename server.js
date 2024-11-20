3105d32c721fa75ff9af72dc273bb7bfbdd51a31
PrinceJoshwa <sprincejoshwa85@gmail.com>
2024-11-20 17:32:57 +0530

    vercel.json changes

--- server.js
+++ server.js
@@ -27,6 +27,7 @@
 
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
